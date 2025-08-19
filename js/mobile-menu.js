867318d660c61109b7cca553a4f8a1d39a52be9c
Davydkin Roman <pogoda2100@gmail.com>
2025-08-19 21:10:07 +0300

    Ready project

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,10 +1,18 @@
+const bodys = document.body;
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuOpenButton = document.querySelector('.header-burger');
-const mobileMenuCloseButton = document.querySelector(
+const mobileMenuCloseButton = document.querySelectorAll(
   '.mobile-menu-close-button',
 );
 
 const toggleMobileMenu = () => mobileMenu.classList.toggle('is-open');
+const toggleBodys = () => bodys.classList.toggle('is-open');
 
 mobileMenuOpenButton.addEventListener('click', toggleMobileMenu);
-mobileMenuCloseButton.addEventListener('click', toggleMobileMenu);
+mobileMenuCloseButton.forEach(button => {
+  button.addEventListener('click', toggleMobileMenu);
+});
+mobileMenuOpenButton.addEventListener('click', toggleBodys);
+mobileMenuCloseButton.forEach(button => {
+  button.addEventListener('click', toggleBodys);
+});
--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,16 @@
+const body = document.body;
 const modal = document.querySelector('.backdrop');
-const modalOpenButton = document.querySelector('.header-button');
+const modalOpenButton = document.querySelectorAll('.header-button');
 const modalCloseButton = document.querySelector('.modal-close-button');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
+const toggleBody = () => body.classList.toggle('is-open');
 
-modalOpenButton.addEventListener('click', toggleModal);
+modalOpenButton.forEach(button => {
+  button.addEventListener('click', toggleModal);
+});
 modalCloseButton.addEventListener('click', toggleModal);
+modalOpenButton.forEach(button => {
+  button.addEventListener('click', toggleBody);
+});
+modalCloseButton.addEventListener('click', toggleBody);
